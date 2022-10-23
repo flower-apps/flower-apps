04cf5a4552ffba3b83489e58d59931b05e14f639
Bassem Khadige <bkhadige@gmail.com>
2022-10-23 17:39:43 +0200

    [static] initial

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container font-sans text-black">
+    <main-navigation></main-navigation>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import MainNavigation from '../components/MainNavigation'
+
+export default {
+  components: {
+    MainNavigation
+  }
+}
+</script>
+
+
+<style>
+  .container-inner {
+    max-width: 650px;
+  }
+
+  .nav.nuxt-link-active {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  
+</template>
+
+<script>
+import NuxtLogo from '../components/NuxtLogo.vue'
+export default {
+  components: { NuxtLogo },
+  name: 'IndexPage'
+}
+</script>
