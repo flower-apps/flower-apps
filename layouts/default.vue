bda82d3fce9d6d8cb0dc8ce11b07d0dd9f355b26
Bassem Khadige <bkhadige@gmail.com>
2022-10-24 07:15:27 +0200

    [static] footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,27 +2,49 @@
   <div class="container font-sans text-black">
     <main-navigation></main-navigation>
     <nuxt />
+    
+
+<footer class="fixed bottom-0 left-0 z-20 p-4 w-2/3 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://MyProject.com/" class="hover:underline">MyProject™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+</footer>
+
+    
   </div>
 </template>
 
 <script>
-import MainNavigation from '../components/MainNavigation'
+import MainNavigation from "../components/MainNavigation";
 
 export default {
   components: {
-    MainNavigation
-  }
-}
+    MainNavigation,
+  },
+};
 </script>
 
 
 <style>
-  .container-inner {
-    max-width: 650px;
-  }
+.container-inner {
+  max-width: 650px;
+}
 
-  .nav.nuxt-link-active {
-    font-weight: bold;
-    text-decoration: underline;
-  }
+.nav.nuxt-link-active {
+  font-weight: bold;
+  text-decoration: underline;
+}
 </style>
